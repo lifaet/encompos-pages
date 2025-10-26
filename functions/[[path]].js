86e12c15477f631d77016528a96a228d4d71f3c3
lifaet <lifaethossain@gmail.com>
2025-10-26 16:03:05 +0600

    .

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,6 +1,6 @@
 export async function onRequest(context) {
   // Replace with the URL you want to redirect to
-  const destination = "https://example.com";
+  const destination = "https://encompos.ddns.net/";
 
   return Response.redirect(destination, 302); // 302 = temporary redirect
 }
